7de6a69eb197e5152e978f8084d10da6e709d980
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-19 18:54:04 -0300

    Add images with carrierwave, S3 and figaro

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ group :development do
 end
 gem 'rails_12factor', group: :production
 gem 'pg', group: :production
+gem 'carrierwave'
+gem "fog-aws"
+gem "figaro"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919175837) do
+ActiveRecord::Schema.define(version: 20170919211016) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170919175837) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "images"
+    t.string "image"
   end
 
 end
