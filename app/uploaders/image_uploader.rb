df6e562f895fd418f4dc2833a4ee9be3085ea56c
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-19 19:16:42 -0300

    Resize images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -26,7 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process :resize_to_fit => [200, 300]
   #
   # def scale(width, height)
   #   # do something
