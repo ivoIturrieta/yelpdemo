30bf96f2c4e0cad3c766cabfa3f8f2a4e99739da
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-19 21:49:13 -0300

    Add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919230941) do
+ActiveRecord::Schema.define(version: 20170920004259) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170919230941) do
     t.datetime "updated_at", null: false
     t.string "images"
     t.string "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
