8e6f803c46eedd8208bace838eaa86510a8eb95b
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-19 22:31:34 -0300

    Remove and and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920004259) do
+ActiveRecord::Schema.define(version: 20170920005712) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170920004259) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
